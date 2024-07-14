122f4abeffcbaa383d6ac33ccc190657fe279662
abubakar9405 <abubakarsid935@gmail.com>
2024-07-15 03:07:59 +0500

    DOUBLY-LINKEDLIST

---
+++ DOUBLY-LINKEDLIST/COMPLETE-DOUBLY.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+//code is'nt uploaded yet just  a testing cpp 
+//will be uploaded soon... (._.)
+int main(){
+    int a=5;
+   int b=10;
+    cout<<a+b;
+    return 0;
+}
