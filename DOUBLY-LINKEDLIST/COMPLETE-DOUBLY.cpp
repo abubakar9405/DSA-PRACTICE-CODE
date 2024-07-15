b53e5222afa81941766e02e405dacd51763668d7
abubakar9405 <abubakarsid935@gmail.com>
2024-07-15 21:02:25 +0500

    SOME CHANGES

--- DOUBLY-LINKEDLIST/COMPLETE-DOUBLY.cpp
+++ DOUBLY-LINKEDLIST/COMPLETE-DOUBLY.cpp
@@ -3,18 +3,9 @@ using namespace std;
 //code is'nt uploaded yet just  a testing cpp 
 //will be uploaded soon... (._.)
 int main(){
-    int a,b,c;
-    cout<<"enter three numbers \n";
-    cin>>a>>b>>c;
-    if (a>c&&a>b)
-    {
-        cout<<a<<" a is the greates\n";
-    }
-    if (b>c&&b>a)
-    {
-        cout<<b<<" b is the greates\n";
-    }
-    else{
-        cout<<c<<" c is the greates number \n";
-    }    
+    int a=5;
+int *ptr=&a;
+cout<<&ptr<<" "<<*ptr<<" "<<ptr;
+cout<<"address of a is \n"<<&a;
+return 0;
 }
