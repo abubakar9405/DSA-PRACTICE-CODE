85e9659547d928f2ba7d071ba2151564624f501d
abubakar9405 <abubakarsid935@gmail.com>
2024-07-21 01:37:36 +0500

    FINALIZED
    ALL POP,PUSH,empty,full,PRINT_STATUS ARE IMPLEMENTED AND TESTED

--- INTRO TO STACK/stackoperations.cpp
+++ INTRO TO STACK/stackoperations.cpp
@@ -1,84 +1,107 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-class stack
-{
 
+class stack {
 public:
-   int size;
-   int top;
-   int *arr;
-//IMPLEMENT TWO FUNC IS_FULL AND IS_EMPTY
-int IS_EMPTY(){
-if (this->top==-1)
-{
-    cout<<"your stack is empty\n";
-    return 1;
-}
-else{
-    cout<<"your stack is not empty\n";
-    return 0;
-}
-}
-int  IS_FULL(){
-if (this->top==this->size-1)
-{
-    cout<<"your stack is full\n";
-    return 1;
-}
-cout<<"your stack is not full\n";
-return 0; 
-}
-void push(int num){
-    stack*temp=this;
-    if (temp->top==temp->size-1)
-    {
-       cout<<"stack overflow\n";
+    int size;
+    int top;
+    int* arr;
+
+    stack(int size) {
+        this->size = size;
+        this->top = -1;
+        this->arr = new int[size];
     }
-    else{
-        temp->top++;
-        temp->arr[this->top]=num;
 
+    ~stack() {
+        delete[] arr;
     }
-}
-void traverse_stack(){
 
-stack*temp=this;
-for (int i = 0; i <temp->size; i++)
-{
-    cout<<temp->arr[i]<<" \n";
-}
-}
+    bool is_empty() {
+        return top == -1;
+    }
 
+    bool is_full() {
+        return top == size - 1;
+    }
+
+    void push(int num) {
+        if (is_full()) {
+            cout << "Stack overflow\n";
+            return;
+        }
+        arr[++top] = num;
+    }
+
+    int pop() {
+        if (is_empty()) {
+            cout << "Stack underflow\n";
+            return -1;
+        }
+        return arr[top--];
+    }
+
+    void traverse_stack() {
+        if (is_empty()) {
+            cout << "Stack is empty\n";
+            return;
+        }
+        for (int i = 0; i <= top; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+
+    void print_status() {
+        cout << "Stack is " << (is_empty() ? "empty" : "not empty") << "\n";
+        cout << "Stack is " << (is_full() ? "full" : "not full") << "\n";
+    }
 };
 
-int main(){
-stack*sp=new stack();
-sp->size=10;//hardcoded size of stack to 10...
-sp->top=-1;//top means the highest element of the stack...
-sp->arr=new int[sp->size];
-cout<<"STACK HAS BEEN CREATED SUCCESSFULLY\n";
-cout<<"checking empty full before updating\n";
-sp->IS_EMPTY();
-sp->IS_FULL();
-int temp_Size;
-cout<<"enter how many element you want to enter in your stack\n";
-cout<<"max capacity of stack for now is 10\n";
-cin>>temp_Size;
-if (temp_Size>10&&temp_Size<1)
-{
-    cout<<"invalid input\n";
-}
-int j;//to input value in the loop for stack ...
-for(int i=0;i<temp_Size;i++){
-  cout<<"enter your value "<<i+1<<" to fill up stack \n";
-   cin>>j;
-   sp->push(j);
-   }
-sp->IS_EMPTY();
-sp->IS_FULL();
-cout<<"traversing through the stack\n";
-sp->traverse_stack();
-return 0;
+int main() {
+    stack* sp = new stack(10);
+    cout << "STACK HAS BEEN CREATED SUCCESSFULLY\n";
+    cout << "Checking empty and full status before updating\n";
+    sp->print_status();
 
+    sp->push(10);
+    sp->push(20);
+    sp->push(30);
+    sp->push(40);
+    sp->push(50);
+    sp->push(60);
+    sp->push(70);
+    sp->push(80);
+    sp->push(90);
+    sp->push(100);
 
+    cout << "TRAVERSING BEFORE POPPING\n";
+    sp->traverse_stack();
+    sp->print_status();
+
+    sp->pop();
+    sp->pop();
+    sp->pop();
+
+    cout << "TRAVERSING AFTER POPPING\n";
+    sp->traverse_stack();
+    sp->print_status();
+
+    delete sp;
+    return 0;
 }
+//IF YOU WANT USER INPUT IN  STACK UNCOMMENT THE CODE GIVEN BELOW...
+//int temp_Size;
+// cout<<"enter how many element you want to enter in your stack\n";
+// cout<<"max capacity of stack for now is 10\n";
+// cin>>temp_Size;
+// if (temp_Size>10&&temp_Size<1)
+// {
+//     cout<<"invalid input\n";
+// }
+// int j;//to input value in the loop for stack ...
+// for(int i=0;i<temp_Size;i++){
+//   cout<<"enter your value "<<i+1<<" to fill up stack \n";
+//    cin>>j;
+//    sp->push(j);
+//    }
---
+++ INTRO TO STACK/tempCodeRunnerFile.cpp
@@ -0,0 +1,4 @@
+if (temp_Size>10&&temp_Size<1)
+// {
+//     cout<<"invalid input\n";
+// }
