9001bbec85698af63ccb44dba5cc72e832e28764
abubakar9405 <abubakarsid935@gmail.com>
2024-07-21 01:15:09 +0500

    MORE FUNC UPDATES

--- INTRO TO STACK/stackoperations.cpp
+++ INTRO TO STACK/stackoperations.cpp
@@ -27,9 +27,26 @@ if (this->top==this->size-1)
 }
 cout<<"your stack is not full\n";
 return 0; 
+}
+void push(int num){
+    stack*temp=this;
+    if (temp->top==temp->size-1)
+    {
+       cout<<"stack overflow\n";
+    }
+    else{
+        temp->top++;
+        temp->arr[this->top]=num;
 
+    }
+}
+void traverse_stack(){
 
-
+stack*temp=this;
+for (int i = 0; i <temp->size; i++)
+{
+    cout<<temp->arr[i]<<" \n";
+}
 }
 
 };
@@ -40,8 +57,27 @@ sp->size=10;//hardcoded size of stack to 10...
 sp->top=-1;//top means the highest element of the stack...
 sp->arr=new int[sp->size];
 cout<<"STACK HAS BEEN CREATED SUCCESSFULLY\n";
+cout<<"checking empty full before updating\n";
 sp->IS_EMPTY();
 sp->IS_FULL();
+int temp_Size;
+cout<<"enter how many element you want to enter in your stack\n";
+cout<<"max capacity of stack for now is 10\n";
+cin>>temp_Size;
+if (temp_Size>10&&temp_Size<1)
+{
+    cout<<"invalid input\n";
+}
+int j;//to input value in the loop for stack ...
+for(int i=0;i<temp_Size;i++){
+  cout<<"enter your value "<<i+1<<" to fill up stack \n";
+   cin>>j;
+   sp->push(j);
+   }
+sp->IS_EMPTY();
+sp->IS_FULL();
+cout<<"traversing through the stack\n";
+sp->traverse_stack();
 return 0;
 
 
